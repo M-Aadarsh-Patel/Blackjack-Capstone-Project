6bf37e6412db610d45796f9eb9933e20a70015aa
M Aadarsh Patel <aadarsh9963355519@gmail.com>
2024-04-29 22:40:19 +0530

    hope fully fixed the bug of computer picking the card twice even though its score is less than 17, and now properly fixed the Blackjack condition

--- Capstone.py
+++ Capstone.py
@@ -52,57 +52,64 @@ while True:
 
         deal_card(cards_to_be_dealt_to=computer_cards, K=2)
         computer_score = caluculate_score(hand=computer_cards)
-        
 
         print(f"Your cards: {player_cards}, Current Score:{current_player_score}")
         print(f"Computer's first card: {computer_cards[0]}")
 
-        card_choice = input("Type 'y' to get another card, type 'n' to pass: ")
+        if player_cards == [10, 11] or player_cards == [11, 10]:
+            print("You've got a Blackjack you win")
 
-        if card_choice.lower() == 'y':
+        elif computer_cards == [10, 11] or computer_cards == [11, 10]:
+            print("Computer has gotten a BlackJack, you lose!")
 
-            #creating a new variable final_player_cards and assiging it to the modified player_cards after appending.
-            deal_card(cards_to_be_dealt_to=player_cards, K=1)
-            final_player_cards = player_cards
+        elif player_cards != [10, 11] and player_cards != [11, 10]:
+            if computer_cards != [10, 11] and player_cards != [11, 10]:
+                card_choice = input("Type 'y' to get another card, type 'n' to pass: ")
 
-            final_player_score = caluculate_score(final_player_cards)
+                if card_choice.lower() == 'y':
 
+                    #creating a new variable final_player_cards and assiging it to the modified player_cards after appending.
+                    deal_card(cards_to_be_dealt_to=player_cards, K=1)
+                    final_player_cards = player_cards
 
-            print(f"Your cards: {final_player_cards}, Final Score: {final_player_score}")
-            print(f"Computer's first card: {computer_cards[0]}")
-
-            print(f"Your final hand: {player_cards}, Final Score: {final_player_score}")
-            
-            print(f"Computer's hand: {computer_cards}, Computer Score: {computer_score}")
-            
-            while computer_score < 17:
-
-                print("The computer had a score less than 17, computer drew a card")
-
-                deal_card(cards_to_be_dealt_to=computer_cards, K=1)
-                final_computer_cards = computer_cards
-
-                final_computer_score = caluculate_score(final_computer_cards)
-                computer_score = final_computer_score
-
-                print(f"Computer's final hand: {final_computer_cards}")
-                
-            else:
-                final_computer_score = computer_score
-
-            result_declaration(computer_final=final_computer_score, player_final=final_player_score)
-
-                
-        if card_choice.lower() == 'n':
-
-            final_player_score = current_player_score
-            final_computer_score = computer_score
-
-            result_declaration(computer_final=final_computer_score, player_final=final_player_score)
+                    final_player_score = caluculate_score(final_player_cards)
 
 
-    if playing.lower() == 'n':
-        break
+                    print(f"Your cards: {final_player_cards}, Final Score: {final_player_score}")
+                    print(f"Computer's first card: {computer_cards[0]}")
+
+                    print(f"Your final hand: {player_cards}, Final Score: {final_player_score}")
+                        
+                    print(f"Computer's hand: {computer_cards}, Computer Score: {computer_score}")
+                        
+                    while computer_score < 17:
+
+                        print("The computer had a score less than 17, computer drew a card")
+
+                        deal_card(cards_to_be_dealt_to=computer_cards, K=1)
+                        final_computer_cards = computer_cards
+
+                        final_computer_score = caluculate_score(final_computer_cards)
+                        computer_score = final_computer_score
+
+                        print(f"Computer's final hand: {final_computer_cards}")
+                            
+                    else:
+                        final_computer_score = computer_score
+
+                    result_declaration(computer_final=final_computer_score, player_final=final_player_score)
+
+                            
+                elif card_choice.lower() == 'n':
+
+                    final_player_score = current_player_score
+                    final_computer_score = computer_score
+
+                    result_declaration(computer_final=final_computer_score, player_final=final_player_score)
+
+
+                elif playing.lower() == 'n':
+                    break
 
 else:
     quit()
