3878a4a5f91f51eab8e8cf71edcfeacbb5ad3f07
M Aadarsh Patel <aadarsh9963355519@gmail.com>
2024-04-29 18:41:53 +0530

    Added a deal_card function which replaced some of the code, added the blackjack rule and tidied up some code

--- Capstone.py
+++ Capstone.py
@@ -2,9 +2,29 @@ import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
+def deal_card(cards_to_be_dealt_to, K):
+    for i in range(K):
+        cards_to_be_dealt_to.append(random.choice(cards))
+
+def caluculate_score(hand):
+    score = sum(hand)
+    if score == 21:
+        return 0
+    else:
+       return score
+
 def result_declaration(computer_final, player_final):
     if player_final < computer_final < 21:
         print(f"The computer had a score of {computer_final}, Your score is {player_final}, the computer won!, You lost")
+
+    elif computer_final and player_final == 0:
+        print("You both had a Blackjack, its a draw!!")
+
+    elif computer_final == 0:
+        print("The computer had a Blackjack. You won!!")
+
+    elif player_final == 0:
+        print("You had a Blackjack. You lost!!")
 
     elif computer_final >= 21 and player_final < 21:
         print(f"The computer had a score {computer_final}, You win!!")
@@ -19,23 +39,19 @@ def result_declaration(computer_final, player_final):
         print(f"It's a draw!! (computer's score: {computer_final}, Your score {player_final})")
 
 
-def caluculate_score(hand):
-    score = sum(hand)
-    if score == 21:
-        return 0
-    else:
-       return score
-
 
 while True:
+
+    player_cards = []
+    computer_cards = []
 
     playing = input("Do you want to play a game of Blackjack? (Type 'y' or 'n') ")
 
     if playing.lower() == 'y':
-        player_cards = random.choices(cards, weights=None, cum_weights=None, k=2)
+        deal_card(cards_to_be_dealt_to=player_cards, K=2)
         current_player_score = caluculate_score(hand=player_cards)
 
-        computer_cards = random.choices(cards, weights=None, cum_weights=None, k=2)
+        deal_card(cards_to_be_dealt_to=computer_cards, K=2)
         computer_score = caluculate_score(hand=computer_cards)
         
 
@@ -45,10 +61,9 @@ while True:
         card_choice = input("Type 'y' to get another card, type 'n' to pass: ")
 
         if card_choice.lower() == 'y':
-            chosen_card = random.choice(cards)
 
             #creating a new variable final_player_cards and assiging it to the modified player_cards after appending.
-            player_cards.append(chosen_card)
+            deal_card(cards_to_be_dealt_to=player_cards, K=1)
             final_player_cards = player_cards
 
             final_player_score = caluculate_score(final_player_cards)
@@ -61,26 +76,24 @@ while True:
             
             print(f"Computer's hand: {computer_cards}, Computer Score: {computer_score}")
             
-            if computer_score < 17:
+            while computer_score < 17:
 
                 print("The computer had a score less than 17, computer drew a card")
 
-                computer_cards.append(random.choice(cards))
+                deal_card(cards_to_be_dealt_to=computer_cards, K=1)
                 final_computer_cards = computer_cards
 
                 final_computer_score = caluculate_score(final_computer_cards)
+                computer_score = final_computer_score
 
                 print(f"Computer's final hand: {final_computer_cards}")
-
                 
-                result_declaration(computer_final=final_computer_score, player_final=final_player_score)
-                    
             else:
                 final_computer_score = computer_score
 
-                result_declaration(computer_final=final_computer_score, player_final=final_player_score)
+            result_declaration(computer_final=final_computer_score, player_final=final_player_score)
 
-               
+                
         if card_choice.lower() == 'n':
 
             final_player_score = current_player_score
